b8876edf8b40ccbb2c02ba60c45dfa9447a76515
MybidAPP <lyp_xianyu@163.com>
2014-08-05 17:49:21 +0800

    threadtest

--- src/thread/hello.java
+++ src/thread/hello.java
@@ -9,7 +9,7 @@ public class hello extends Thread{
 		// TODO Auto-generated method stub
 		super.run();
 		for(int i=0;i<5;i++)
-		System.out.printf(name+i+"hello world\n");
+		System.out.printf(name+i+"hello worldtest\n");
 	}
    public static void main(String str[])
    {
